1dcb21dbb4f27411f7602fcc88aca7790bd065f2
AEscobarAngola <abrancho1908@gmail.com>
2024-06-05 21:38:38 +0200

    feat: setting up buttons w js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let mainButton = document.getElementById("turn-button-section")
+let interruptor = false
+let allButtons = document.getElementsByClassName("general-buttons")
+let arrayButtons = Array.from(allButtons)
+
+mainButton.addEventListener("click", () => {
+     interruptor = !interruptor
+     console.log(interruptor)
+})
+
+for(let i = 0; i <= allButtons.length; i++){
+    allButtons[i].addEventListener("click", () =>{
+        interruptor
+        ?console.log(`Estás viendo el canal ${i}`)
+        :console.log("la tv está apagada")
+    })
+}
