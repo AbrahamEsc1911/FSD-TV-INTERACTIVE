f67499bd57b1443902bb25d9a2b726f259950041
AEscobarAngola <abrancho1908@gmail.com>
2024-06-06 09:45:45 +0200

    feat: led on-off done

--- js/main.js
+++ js/main.js
@@ -2,15 +2,14 @@ let mainButton = document.getElementById("turn-button-section")
 let interruptor = false
 let allButtons = document.getElementsByClassName("general-buttons")
 let arrayButtons = Array.from(allButtons)
-let ligthOnOff = document.getElementsByClassName("tv-ligth-turned-off")
+let ligthOnOff = document.getElementById("tv-led")
 
 
 mainButton.addEventListener("click", () => {
-    ligthOnOff.classlist.toggle("tv-ligth-turned-on")  
+    ligthOnOff.classList.toggle("tv-ligth-turned-on")
     interruptor = !interruptor
     console.log(interruptor)
 })
- 
 for(let i = 0; i <= allButtons.length; i++){
     allButtons[i].addEventListener("click", () =>{
         interruptor
