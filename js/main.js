6bc953f5e6df49401716ae91ef8ae9c941c27a58
AEscobarAngola <abrancho1908@gmail.com>
2024-06-06 22:52:37 +0200

    feat: buttons workin, numbers, on off, netflix btn

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
-let mainButton = document.getElementById("turn-button-section")
-let interruptor = false
-let allButtons = document.getElementsByClassName("general-buttons")
-let arrayButtons = Array.from(allButtons)
-let ligthOnOff = document.getElementById("tv-led")
+let onOfButton = document.getElementById("turn-button-section");
+let interruptor = false;
+let tvLed = document.getElementById("tv-led");
+let btns = document.getElementsByClassName("general-buttons");
+let arrayBtns = Array.from(btns);
+
+let netflix = document.getElementById("netflix-btn")
+
+let time = new Date;
+
+let screen = document.getElementById("Screen");
 
 
-mainButton.addEventListener("click", () => {
-    ligthOnOff.classList.toggle("tv-ligth-turned-on")
-    interruptor = !interruptor
-    console.log(interruptor)
+
+
+
+onOfButton.addEventListener("click", () => {
+    interruptor = !interruptor;
+    tvLed.classList.toggle("tv-led-on")
+    screen.innerHTML = `<img id="size-screen" src="./img/ch1.gif" alt="inicial">`
+    !interruptor
+    ?screen.innerHTML = ``
+    : null
 })
-for(let i = 0; i <= allButtons.length; i++){
-    allButtons[i].addEventListener("click", () =>{
+
+for(let i = 0; i<arrayBtns.length; i++){
+    arrayBtns[i].addEventListener("click", () => {
         interruptor
-        ?console.log(`Estás viendo el canal ${i}`)
-        :console.log("la tv está apagada")
+        ?screen.innerHTML = `<img id="size-screen" src="./img/ch${i+1}.gif" alt="inicial"><div id="Canal">Canal ${i+1}</div>`
+        :null
     })
-}
+
+    }
+
+    netflix.addEventListener("click", () => {
+        interruptor
+        ?screen.innerHTML = `<img id="size-screen" src="./img/net.gif" alt="inicial">`
+        :null
+    })
+
