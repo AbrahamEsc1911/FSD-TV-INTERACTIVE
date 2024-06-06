04ccb8ae873bffd5eb04b8e463aa47271d422a1b
AEscobarAngola <abrancho1908@gmail.com>
2024-06-06 23:58:10 +0200

    feat: buttons working, numbers, chanel up and down, power on off, netflix

--- js/main.js
+++ js/main.js
@@ -4,37 +4,60 @@ let tvLed = document.getElementById("tv-led");
 let btns = document.getElementsByClassName("general-buttons");
 let arrayBtns = Array.from(btns);
 
-let netflix = document.getElementById("netflix-btn")
-
-let time = new Date;
+let netflix = document.getElementById("netflix-btn");
 
 let screen = document.getElementById("Screen");
+let chanelUp = document.getElementById("chanel-up")
+let countUp = 0;
 
-
+let chanelDown = document.getElementById("chanel-down")
 
 
 
 onOfButton.addEventListener("click", () => {
     interruptor = !interruptor;
     tvLed.classList.toggle("tv-led-on")
-    screen.innerHTML = `<img id="size-screen" src="./img/ch1.gif" alt="inicial">`
+    screen.innerHTML = `<img id="size-screen" src="./img/on.gif" alt="inicial">`
     !interruptor
-    ?screen.innerHTML = ``
-    : null
+        ? screen.innerHTML = ``
+        : null
 })
 
-for(let i = 0; i<arrayBtns.length; i++){
+for (let i = 0; i < arrayBtns.length; i++) {
     arrayBtns[i].addEventListener("click", () => {
         interruptor
-        ?screen.innerHTML = `<img id="size-screen" src="./img/ch${i+1}.gif" alt="inicial"><div id="Canal">Canal ${i+1}</div>`
-        :null
+            ? screen.innerHTML = `<img id="size-screen" src="./img/ch${i + 1}.gif" alt="inicial"><div id="Canal">Canal ${i + 1}</div>`
+            : null
     })
+}
 
+netflix.addEventListener("click", () => {
+    interruptor
+        ? screen.innerHTML = `<img id="size-screen" src="./img/net.gif" alt="inicial">`
+        : null
+})
+
+
+chanelUp.addEventListener("click", () => {
+
+    if (interruptor && countUp <= 5) {
+        countUp += 1
+        screen.innerHTML = `<img id="size-screen" src="./img/ch${countUp}.gif" alt="inicial"><div id="Canal">Canal ${countUp}</div>`
+    } else if (interruptor && countUp >= 5) {
+        screen.innerHTML = `<img id="size-screen" src="./img/ch${countUp}.gif" alt="inicial"><div id="Canal">Canal ${countUp}</div>`
+    } else {
+        null
     }
 
-    netflix.addEventListener("click", () => {
-        interruptor
-        ?screen.innerHTML = `<img id="size-screen" src="./img/net.gif" alt="inicial">`
-        :null
-    })
+})
 
+chanelDown.addEventListener("click", () => {
+    if (interruptor && countUp >= 2) {
+        countUp -= 1 >= 1
+        screen.innerHTML = `<img id="size-screen" src="./img/ch${countUp}.gif" alt="inicial"><div id="Canal">Canal ${countUp}</div>`
+    } else if (interruptor && countUp <= 2) {
+        screen.innerHTML = `<img id="size-screen" src="./img/ch${countUp}.gif" alt="inicial"><div id="Canal">Canal ${countUp}</div>`
+    } else {
+        null
+    }
+})
