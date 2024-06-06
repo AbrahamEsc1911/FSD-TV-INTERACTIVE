e8bf9e00f23ae17ce8dc6aedac5342977e12bf60
AEscobarAngola <abrancho1908@gmail.com>
2024-06-06 09:20:54 +0200

    feat: help

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ let mainButton = document.getElementById("turn-button-section")
 let interruptor = false
 let allButtons = document.getElementsByClassName("general-buttons")
 let arrayButtons = Array.from(allButtons)
+let ligthOnOff = document.getElementsByClassName("tv-ligth-turned-off")
+
 
 mainButton.addEventListener("click", () => {
-     interruptor = !interruptor
-     console.log(interruptor)
+    ligthOnOff.classlist.toggle("tv-ligth-turned-on")  
+    interruptor = !interruptor
+    console.log(interruptor)
 })
-
+ 
 for(let i = 0; i <= allButtons.length; i++){
     allButtons[i].addEventListener("click", () =>{
         interruptor
